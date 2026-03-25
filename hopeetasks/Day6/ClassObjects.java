f909885ad981afc3a6792337aa8f65fe1184c8f2
MEENA DEVI T <meenadevit567@gmail.com>
2026-03-25 14:46:41 +0530

    Add files via upload

---
+++ hopeetasks/Day6/ClassObjects.java
@@ -0,0 +1,18 @@
+package Day6;
+public class ClassObjects {
+    public static void main(String[] args) {
+        System.out.println("I am inside Class Objects");
+        Apartment apt=new Apartment();
+        apt.display();
+        
+    }
+    
+}
+ class Apartment{
+    void display() {
+        System.out.print("I an inside apartment");
+    }
+
+}
+
+
