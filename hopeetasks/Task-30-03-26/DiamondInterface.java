6699877ab62431a2ac6543931f29feb7cb723a04
MEENA DEVI T <meenadevit567@gmail.com>
2026-03-30 21:58:49 +0530

    Add files via upload

---
+++ hopeetasks/Task-30-03-26/DiamondInterface.java
@@ -0,0 +1,24 @@
+import java. util.*;
+    public class DiamondInterface{
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.show();
+    }
+}
+interface A {
+    default void show() {
+        System.out.println("A's show() method");
+    }
+}
+interface B {
+    default void show() {
+        System.out.println("B's show() method");
+    }
+}
+class C implements A, B {
+    public void show() {
+        System.out.println("Resolving conflict in class C");   
+        A.super.show();
+        B.super.show();
+    }
+}
