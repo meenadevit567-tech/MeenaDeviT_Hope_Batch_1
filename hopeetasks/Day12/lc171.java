fd56d7f1d04724525eb1bda42d83ae346eea1ba7
MEENA DEVI T <meenadevit567@gmail.com>
2026-04-04 09:58:23 +0530

    Add files via upload

---
+++ hopeetasks/Day12/lc171.java
@@ -0,0 +1,18 @@
+package Day12;
+import java.util.*;
+
+public class lc171 {
+    public static int titleToNumber(String columnTitle) {
+        int r=0;
+        for(char ch:columnTitle.toCharArray()){
+            r=r*26+(ch-'A'+1);
+        }
+        return r;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(titleToNumber(s));
+        sc.close();
+    }
+}
